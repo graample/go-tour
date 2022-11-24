45a59373c62d270b997db92a680d4661688a6ab9
Raymond Yee <haesik.yee@gmail.com>
2022-11-23 21:16:09 -0800

    adds exercises from section 7

---
+++ handsOnExercises/section7/exercise2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 3
+	fmt.Println(x == 3)
+	fmt.Println(x <= 4)
+	fmt.Println(x >= 2)
+	fmt.Println(x != 4)
+	fmt.Println(x < 4)
+	fmt.Println(x > 2)
+}
---
+++ handsOnExercises/section7/exercise5.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := `
+	sup
+	my
+	"guy"
+	?`
+	fmt.Println(a)
+}
